fd6a2cac236e2ae332bea32de6005a9d2bfb67bc
Aaron <hendulangzi@126.com>
2016-05-05 22:39:46 +0800

    autocheck.sh
    'autocheck.sh' for linux

---
+++ autocheck.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+php5 /home/sole/autoLogin.php Aaron 18788577839 >>/home/sole/logs/autoCheck/`date "+%Y%m%d"`Login.log
+
+php5 /home/sole/autoLogin.php duyan 270023 >>/home/sole/logs/autoCheck/`date "+%Y%m%d"`Login.log
+
